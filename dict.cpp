927ce2a46e696c23a4538a6ffa0021cd0557e6ca
cglosner <cglosner@gmail.com>
2021-04-01 17:33:44 -0400

    Add files via upload

--- dict.cpp
+++ dict.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include <fstream>
-#include "bTree.h"
+#include <iterator>
+#include <map>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <bits/stdc++.h>
+#include <sys/times.h>
+#include <time.h>
+#include "sys/types.h"
+#include "sys/sysinfo.h"
 
-
-bTree BT;
+std::map<std::string, std::vector<long int>> dictionary;
+std::map<long int, std::string> list;
 
 //This uncompresses the document
 void extract(std::string inputName)
@@ -11,12 +21,45 @@ void extract(std::string inputName)
   std::ifstream file(inputName, std::ios_base::in);
   if(file.is_open())
   {
-    std::string s;
-    while(getline(file, s))
+    std::string line;
+    while(getline(file, line))
     {
-
+      std::istringstream ss(line);
+      std::string s;
+      std::string word;
+      std::vector<long int> tmp;
+      bool first = true;
+      while(ss >> s)
+      {
+        if(first)
+        {
+          word = s;
+          first = false;
+        }
+        else
+        {
+          list.insert(std::pair<long int, std::string>((long int)std::stoi(s), word));
+          tmp.push_back((long int)std::stoi(s));
+        }
+      }
+      dictionary.insert(std::pair<std::string, std::vector<long int>>(word, tmp));
     }
     file.close();
+  }
+}
+
+//write the decoded data from the specially
+//constructed list
+void writeDecoded(std::string outputName)
+{
+  std::ofstream out_file(outputName, std::ios::trunc | std::ios_base::out);
+  if(out_file.is_open())
+  {
+    for(int i = 0; i < list.size(); i++)
+    {
+      out_file << list[i] << std::endl;
+    }
+    out_file.close();
   }
 }
 
@@ -27,25 +70,56 @@ void locate(std::string inputName)
   if(file.is_open())
   {
     std::string s;
+    long int line = 0;
     while(getline(file, s))
     {
-      btInsert(BT, std::stoi(s));
+      if(dictionary.count(s) > 0)
+      {
+        dictionary.at(s).push_back(line);
+      }
+      else
+      {
+        std::vector<long int> tmp{line};
+        dictionary.insert(std::pair<std::string, std::vector<long int>>(s, tmp));
+      }
+      line++;
     }
     file.close();
   }
 }
 
-
-void writeFile(std::string outputName)
+//write the encoded data
+void writeEncoded(std::string outputName)
 {
   std::ofstream out_file(outputName, std::ios::trunc | std::ios_base::out);
   if(out_file.is_open())
   {
-    //while(decomp >> size)
-    //{
-    //  out_file << comp;
-    //}
+    std::map<std::string, std::vector<long int>>::iterator itr;
+    for(itr = dictionary.begin(); itr != dictionary.end(); itr++)
+    {
+      out_file << itr->first << " ";
+      std::vector<long int> tmp = itr->second;
+      for(int i = 0; i < tmp.size(); i++)
+      {
+        out_file << tmp[i] << " ";
+      }
+      out_file << std::endl;
+    }
     out_file.close();
+  }
+}
+
+//Locate the number of occurances of a word
+//from dictionary
+void query(std::string data)
+{
+  if(dictionary.count(data) > 0)
+  {
+    std::cout << data << " had " << dictionary.at(data).size() << " occurances." << std::endl;
+  }
+  else
+  {
+    std::cout << "Data isn't contained in file" << std::endl;
   }
 }
 
@@ -58,28 +132,46 @@ int main(int argc, char *argv[])
 
   if(argc == 4)
   {
-    BT = btCreate();
     if(std::string(argv[1]) == "-e")
     {
+      auto start = std::chrono::high_resolution_clock::now();
       locate(std::string(argv[2]));
-      writeFile(std::string(argv[3]));
+      writeEncoded(std::string(argv[3]));
+      auto stop = std::chrono::high_resolution_clock::now();
+      double duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+      duration /= 1e6;
+      std::cout << "Encode Runtime: " << std::fixed << duration << std::setprecision(9);
+      std::cout << "sec" << std::endl;
     }
     else if(std::string(argv[1]) == "-d")
     {
+      auto start = std::chrono::high_resolution_clock::now();
       extract(std::string(argv[2]));
-      writeFile(std::string(argv[3]));
+      writeDecoded(std::string(argv[3]));
+      auto stop = std::chrono::high_resolution_clock::now();
+      double duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+      duration /= 1e6;
+      std::cout << "Decode Runtime: " << std::fixed << duration << std::setprecision(9);
+      std::cout << "sec" << std::endl;
     }
-    btDestroy(BT);
   }
 
   //Arguements:
   //argv[1] - query
-  //argv[2] - data
-  if(argc == 3)
+  //argv[2] - file
+  //argv[3] - data
+  if(argc == 4)
   {
     if(std::string(argv[1]) == "-q")
     {
-
+      auto start = std::chrono::high_resolution_clock::now();
+      extract(std::string(argv[2]));
+      query(std::string(argv[3]));
+      auto stop = std::chrono::high_resolution_clock::now();
+      double duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+      duration /= 1e6;
+      std::cout << "Query Runtime: " << std::fixed << duration << std::setprecision(9);
+      std::cout << "sec" << std::endl;
     }
   }
 }
